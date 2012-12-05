8ffca8dd12983b0f3d9ca1d016aa65c21b0efa2c
Ben Iofel <iofelben@gmail.com>
2012-12-05 10:33:24 -0500

    update

--- n-queens geneticalgorithm/n-queens geneticalgorithm/main.c
+++ n-queens geneticalgorithm/n-queens geneticalgorithm/main.c
@@ -49,61 +49,51 @@ void geneticAlgorithm(){
     int done = 0;
     int i;
     for(i=0;i<POP;i++) for(int j=0;j<SIZE;j++) population[i][j] = getRand(SIZE);
-    //    while(done == 0){
-    
-    for(i=0;i<POP;i++){
-        if(getWeight(children[i]) == 28){
-            printf("solution: ");
-            printArray(children[i]);
-            done = 1;
+    while(done == 0){        
+        for(i=0;i<POP;i++){
+            if(getWeight(children[i]) == 28){
+                printf("solution: ");
+                printArray(children[i]);
+                done = 1;
+            }
         }
+        
+        for(i=0;i<wpl;i++) weightProb[i] = (int)NULL; //clear weightprob
+        wpl=0;
+        
+        //weighted probability distribution
+        for(i=0;i<POP;i++){
+            int w = getWeight(population[i]);
+            for(int j=0;j<w;j++){
+                weightProb[wpl] = i; //fill array with member number w times
+                wpl++;
+            }
+        }
+        
+        //reproduce
+        for(i=0;i<POP;i+=2){
+            int par1 = weightProb[getRand(wpl)];
+            int par2 = weightProb[getRand(wpl)];
+            int split = getRand(SIZE);
+            //crossover
+            for(int j=0;j<split;j++){
+                children[i][j] = population[par1][j];
+                children[i+1][j] = population[par2][j];
+            }
+            for(int j=split;j<SIZE;j++){
+                children[i][j] = population[par2][j];
+                children[i+1][j] = population[par1][j];
+            }
+            //mutation
+            if(getRand(1000000)<=mutProb*1000000){
+                int child=getRand(2);
+                if(child == 0) children[i][getRand(SIZE)] = getRand(SIZE);
+                else children[i+1][getRand(SIZE)] = getRand(SIZE);
+            }
+        }
+        for(i=0;i<POP;i++) for(int j=0;j<SIZE;j++) population[i][j] = children[i][j];
+        wpl = 0;
     }
-    
-    for(i=0;i<wpl;i++) weightProb[i] = (int)NULL; //clear weightprob
-    wpl=0;
-    
-    //weighted probability distribution
-    for(i=0;i<POP;i++){
-        int w = getWeight(population[i]);
-        
-        printf("\n\n\npopulation:\n");
-        for(int i=0;i<POP;i++) printArray(population[i]);
-        
-        for(int j=0;j<w;j++){
-            weightProb[wpl] = i; //fill array with member number [weight] times
-            wpl++;
-        }
-        
-        
-        printf("population:\n");
-        for(int i=0;i<POP;i++) printArray(population[i]);
-        
-    }
-    
-    //reproduce
-    for(i=0;i<POP;i+=2){
-        int par1 = weightProb[getRand(wpl)];
-        int par2 = weightProb[getRand(wpl)];
-        int split = getRand(SIZE);
-        //crossover
-        for(int j=0;j<split;j++){
-            children[i][j] = population[par1][j];
-            children[i+1][j] = population[par2][j];
-        }
-        for(int j=split;j<SIZE;j++){
-            children[i][j] = population[par2][j];
-            children[i+1][j] = population[par1][j];
-        }
-        //mutation
-        if(getRand(1000000)<=mutProb*1000000){
-            int child=getRand(2);
-            if(child == 0) children[i][getRand(SIZE)] = getRand(SIZE);
-            else children[i+1][getRand(SIZE)] = getRand(SIZE);
-        }
-    }
-    for(i=0;i<POP;i++) for(int j=0;j<SIZE;j++) population[i][j] = children[i][j];
-    wpl = 0;
-    //    }
 }
 
 int main(int argc, const char * argv[]){
